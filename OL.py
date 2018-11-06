d6126b1f28d5fabcc9ab434eeace1a9e30560eb5
Miguel Domingo <midobal@prhlt.upv.es>
2018-11-06 11:45:09 +0100

    Online learning first version.

---
+++ OL.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+"""
+    Main training workflow
+"""
+from __future__ import division
+import argparse
+import os
+import signal
+import torch
+
+import onmt.opts as opts
+import onmt.utils.distributed
+
+from onmt.train_single import main as single_main
+from onmt.inputters.inputter import build_dataset_iter, lazily_load_dataset, \
+    _load_fields, _collect_report_features
+from onmt.model_builder import build_model
+from onmt.utils.optimizers import build_optim
+from onmt.trainer import build_trainer
+from onmt.models import build_model_saver
+from onmt.utils.logging import init_logger, logger
+from onmt.train_single import training_opt_postprocessing, _tally_parameters, _check_save_model_path
+
+
+def main(opt):
+
+    if len(opt.gpu_ranks) == 1:  # case 1 GPU only
+        device_id = 0
+    else:   # case only CPU
+        device_id = -1
+
+    opt = training_opt_postprocessing(opt, device_id)
+    init_logger(opt.log_file)
+
+    # Load model.
+    logger.info('Loading checkpoint from %s' % opt.model)
+    checkpoint = torch.load(opt.model, map_location=lambda storage, loc: storage)
+    model_opt = checkpoint['opt']
+
+    # Peek the first dataset to determine the data_type.
+    # (All datasets have the same data_type).
+    first_dataset = next(lazily_load_dataset("train", opt))
+    data_type = first_dataset.data_type
+
+    # Load fields generated from preprocess phase.
+    fields = _load_fields(first_dataset, data_type, opt, checkpoint)
+
+    # Report src/tgt features.
+
+    src_features, tgt_features = _collect_report_features(fields)
+    for j, feat in enumerate(src_features):
+        logger.info(' * src feature %d size = %d'
+                    % (j, len(fields[feat].vocab)))
+    for j, feat in enumerate(tgt_features):
+        logger.info(' * tgt feature %d size = %d'
+                    % (j, len(fields[feat].vocab)))
+
+    # Build model.
+    model = build_model(model_opt, opt, fields, checkpoint)
+    n_params, enc, dec = _tally_parameters(model)
+    logger.info('encoder: %d' % enc)
+    logger.info('decoder: %d' % dec)
+    logger.info('* number of parameters: %d' % n_params)
+    _check_save_model_path(opt)
+
+    # Build optimizer.
+    optim = build_optim(model, opt, checkpoint)
+
+    # Build model saver
+    model_saver = build_model_saver(model_opt, opt, model, fields, optim)
+
+    trainer = build_trainer(opt, device_id, model, fields,
+                            optim, data_type, model_saver=model_saver)
+
+    def train_iter_fct():
+        return build_dataset_iter(
+            lazily_load_dataset("train", opt), fields, opt)
+
+    def valid_iter_fct():
+        return build_dataset_iter(
+            lazily_load_dataset("valid", opt), fields, opt, is_train=False)
+
+    # Do training.
+    if len(opt.gpu_ranks):
+        logger.info('Starting training on GPU: %s' % opt.gpu_ranks)
+    else:
+        logger.info('Starting training on CPU, could be very slow')
+    trainer.train(train_iter_fct, valid_iter_fct, opt.train_steps,
+                  opt.valid_steps)
+
+    if opt.tensorboard:
+        trainer.report_manager.tensorboard_writer.close()
+
+
+def run(opt, device_id, error_queue):
+    """ run process """
+    try:
+        gpu_rank = onmt.utils.distributed.multi_init(opt, device_id)
+        if gpu_rank != opt.gpu_ranks[device_id]:
+            raise AssertionError("An error occurred in \
+                  Distributed initialization")
+        single_main(opt, device_id)
+    except KeyboardInterrupt:
+        pass  # killed by parent, do nothing
+    except Exception:
+        # propagate exception to parent process, keeping original traceback
+        import traceback
+        error_queue.put((opt.gpu_ranks[device_id], traceback.format_exc()))
+
+
+class ErrorHandler(object):
+    """A class that listens for exceptions in children processes and propagates
+    the tracebacks to the parent process."""
+
+    def __init__(self, error_queue):
+        """ init error handler """
+        import signal
+        import threading
+        self.error_queue = error_queue
+        self.children_pids = []
+        self.error_thread = threading.Thread(
+            target=self.error_listener, daemon=True)
+        self.error_thread.start()
+        signal.signal(signal.SIGUSR1, self.signal_handler)
+
+    def add_child(self, pid):
+        """ error handler """
+        self.children_pids.append(pid)
+
+    def error_listener(self):
+        """ error listener """
+        (rank, original_trace) = self.error_queue.get()
+        self.error_queue.put((rank, original_trace))
+        os.kill(os.getpid(), signal.SIGUSR1)
+
+    def signal_handler(self, signalnum, stackframe):
+        """ signal handler """
+        for pid in self.children_pids:
+            os.kill(pid, signal.SIGINT)  # kill children processes
+        (rank, original_trace) = self.error_queue.get()
+        msg = """\n\n-- Tracebacks above this line can probably
+                 be ignored --\n\n"""
+        msg += original_trace
+        raise Exception(msg)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='OL.py',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    opts.add_md_help_argument(parser)
+    opts.model_opts(parser)
+    opts.OL_opts(parser)
+
+    opt = parser.parse_args()
+    main(opt)
--- onmt/opts.py
+++ onmt/opts.py
@@ -446,6 +446,288 @@ def train_opts(parser):
                        help="""Using grayscale image can training
                        model faster and smaller""")
 
+def OL_opts(parser):
+    """ Online Learning options (training and translating) """
+
+    group = parser.add_argument_group('General')
+    group.add_argument('-data', required=True,
+                       help="""Path prefix to the ".train.pt" and
+                       ".valid.pt" file path from preprocess.py""")
+
+    group.add_argument('-save_model', default='model',
+                       help="""Model filename (the model will be saved as
+                       <save_model>_N.pt where N is the number
+                       of steps""")
+
+    group.add_argument('-save_checkpoint_steps', type=int, default=1000,
+                       help="""Save a checkpoint every X steps""")
+    group.add_argument('-keep_checkpoint', type=int, default=-1,
+                       help="""Keep X checkpoints (negative: keep all)""")
+
+    # GPU
+    group.add_argument('-gpuid', default=[], nargs='+', type=int,
+                       help="Deprecated see world_size and gpu_ranks.")
+    group.add_argument('-gpu_ranks', default=[], nargs='+', type=int,
+                       help="list of ranks of each process.")
+    group.add_argument('-world_size', default=1, type=int,
+                       help="total number of distributed processes.")
+    group.add_argument('-gpu_backend', default='nccl', nargs='+', type=str,
+                       help="Type of torch distributed backend")
+    group.add_argument('-gpu_verbose_level', default=0, type=int,
+                       help="Gives more info on each process per GPU.")
+    group.add_argument('-master_ip', default="localhost", type=str,
+                       help="IP of master for torch.distributed training.")
+    group.add_argument('-master_port', default=10000, type=int,
+                       help="Port of master for torch.distributed training.")
+
+    group.add_argument('-seed', type=int, default=-1,
+                       help="""Random seed used for the experiments
+                       reproducibility.""")
+
+    # Init options
+    group = parser.add_argument_group('Initialization')
+    group.add_argument('-param_init', type=float, default=0.1,
+                       help="""Parameters are initialized over uniform distribution
+                       with support (-param_init, param_init).
+                       Use 0 to not use initialization""")
+    group.add_argument('-param_init_glorot', action='store_true',
+                       help="""Init parameters with xavier_uniform.
+                       Required for transfomer.""")
+
+    group.add_argument('-train_from', default='', type=str,
+                       help="""If training from a checkpoint then this is the
+                       path to the pretrained model's state_dict.""")
+    group.add_argument('-reset_optim', default='none',
+                       choices=['none', 'all', 'states', 'keep_states'],
+                       help="""Optimization resetter when train_from.""")
+
+    # Pretrained word vectors
+    group.add_argument('-pre_word_vecs_enc',
+                       help="""If a valid path is specified, then this will load
+                       pretrained word embeddings on the encoder side.
+                       See README for specific formatting instructions.""")
+    group.add_argument('-pre_word_vecs_dec',
+                       help="""If a valid path is specified, then this will load
+                       pretrained word embeddings on the decoder side.
+                       See README for specific formatting instructions.""")
+    # Fixed word vectors
+    group.add_argument('-fix_word_vecs_enc',
+                       action='store_true',
+                       help="Fix word embeddings on the encoder side.")
+    group.add_argument('-fix_word_vecs_dec',
+                       action='store_true',
+                       help="Fix word embeddings on the decoder side.")
+
+    # Optimization options
+    group = parser.add_argument_group('Optimization- Type')
+    group.add_argument('-batch_size', type=int, default=64,
+                       help='Maximum batch size for training')
+    group.add_argument('-batch_type', default='sents',
+                       choices=["sents", "tokens"],
+                       help="""Batch grouping for batch_size. Standard
+                               is sents. Tokens will do dynamic batching""")
+    group.add_argument('-normalization', default='sents',
+                       choices=["sents", "tokens"],
+                       help='Normalization method of the gradient.')
+    group.add_argument('-accum_count', type=int, default=1,
+                       help="""Accumulate gradient this many times.
+                       Approximately equivalent to updating
+                       batch_size * accum_count batches at once.
+                       Recommended for Transformer.""")
+    group.add_argument('-valid_steps', type=int, default=10000,
+                       help='Perfom validation every X steps')
+    group.add_argument('-valid_batch_size', type=int, default=32,
+                       help='Maximum batch size for validation')
+    group.add_argument('-max_generator_batches', type=int, default=32,
+                       help="""Maximum batches of words in a sequence to run
+                        the generator on in parallel. Higher is faster, but
+                        uses more memory.""")
+    group.add_argument('-train_steps', type=int, default=100000,
+                       help='Number of training steps')
+    group.add_argument('-epochs', type=int, default=0,
+                       help='Deprecated epochs see train_steps')
+    group.add_argument('-optim', default='sgd',
+                       choices=['sgd', 'adagrad', 'adadelta', 'adam',
+                                'sparseadam'],
+                       help="""Optimization method.""")
+    group.add_argument('-adagrad_accumulator_init', type=float, default=0,
+                       help="""Initializes the accumulator values in adagrad.
+                       Mirrors the initial_accumulator_value option
+                       in the tensorflow adagrad (use 0.1 for their default).
+                       """)
+    group.add_argument('-max_grad_norm', type=float, default=5,
+                       help="""If the norm of the gradient vector exceeds this,
+                       renormalize it to have the norm equal to
+                       max_grad_norm""")
+    group.add_argument('-dropout', type=float, default=0.3,
+                       help="Dropout probability; applied in LSTM stacks.")
+    group.add_argument('-truncated_decoder', type=int, default=0,
+                       help="""Truncated bptt.""")
+    group.add_argument('-adam_beta1', type=float, default=0.9,
+                       help="""The beta1 parameter used by Adam.
+                       Almost without exception a value of 0.9 is used in
+                       the literature, seemingly giving good results,
+                       so we would discourage changing this value from
+                       the default without due consideration.""")
+    group.add_argument('-adam_beta2', type=float, default=0.999,
+                       help="""The beta2 parameter used by Adam.
+                       Typically a value of 0.999 is recommended, as this is
+                       the value suggested by the original paper describing
+                       Adam, and is also the value adopted in other frameworks
+                       such as Tensorflow and Kerras, i.e. see:
+                       https://www.tensorflow.org/api_docs/python/tf/train/AdamOptimizer
+                       https://keras.io/optimizers/ .
+                       Whereas recently the paper "Attention is All You Need"
+                       suggested a value of 0.98 for beta2, this parameter may
+                       not work well for normal models / default
+                       baselines.""")
+    group.add_argument('-label_smoothing', type=float, default=0.0,
+                       help="""Label smoothing value epsilon.
+                       Probabilities of all non-true labels
+                       will be smoothed by epsilon / (vocab_size - 1).
+                       Set to zero to turn off label smoothing.
+                       For more detailed information, see:
+                       https://arxiv.org/abs/1512.00567""")
+    # learning rate
+    group = parser.add_argument_group('Optimization- Rate')
+    group.add_argument('-learning_rate', type=float, default=1.0,
+                       help="""Starting learning rate.
+                       Recommended settings: sgd = 1, adagrad = 0.1,
+                       adadelta = 1, adam = 0.001""")
+    group.add_argument('-learning_rate_decay', type=float, default=0.5,
+                       help="""If update_learning_rate, decay learning rate by
+                       this much if (i) perplexity does not decrease on the
+                       validation set or (ii) steps have gone past
+                       start_decay_steps""")
+    group.add_argument('-start_decay_steps', type=int, default=50000,
+                       help="""Start decaying every decay_steps after
+                       start_decay_steps""")
+    group.add_argument('-decay_steps', type=int, default=10000,
+                       help="""Decay every decay_steps""")
+
+    group.add_argument('-decay_method', type=str, default="",
+                       choices=['noam'], help="Use a custom decay rate.")
+    group.add_argument('-warmup_steps', type=int, default=4000,
+                       help="""Number of warmup steps for custom decay.""")
+
+    group = parser.add_argument_group('Logging')
+    group.add_argument('-report_every', type=int, default=1,
+                       help="Print stats at this interval.")
+    group.add_argument('-log_file', type=str, default="",
+                       help="Output logs to a file under this path.")
+    group.add_argument('-exp_host', type=str, default="",
+                       help="Send logs to this crayon server.")
+    group.add_argument('-exp', type=str, default="",
+                       help="Name of the experiment for logging.")
+    # Use TensorboardX for visualization during training
+    group.add_argument('-tensorboard', action="store_true",
+                       help="""Use tensorboardX for visualization during training.
+                       Must have the library tensorboardX.""")
+    group.add_argument("-tensorboard_log_dir", type=str,
+                       default="runs/onmt",
+                       help="""Log directory for Tensorboard.
+                       This is also the name of the run.
+                       """)
+    ###############
+    # Translating #
+    ###############
+    group = parser.add_argument_group('Model')
+    group.add_argument('-model', dest='models', metavar='MODEL',
+                       nargs='+', type=str, default=[], required=True,
+                       help='Path to model .pt file(s). '
+                            'Multiple models can be specified, '
+                            'for ensemble decoding.')
+
+    group = parser.add_argument_group('Data')
+    group.add_argument('-data_type', default="text",
+                       help="Type of the source input. Options: [text|img].")
+
+    group.add_argument('-src', required=True,
+                       help="""Source sequence to decode (one line per
+                           sequence)""")
+    group.add_argument('-src_dir', default="",
+                       help='Source directory for image or audio files')
+    group.add_argument('-tgt',
+                       help='True target sequence (optional)')
+    group.add_argument('-output', default='pred.txt',
+                       help="""Path to output the predictions (each line will
+                           be the decoded sequence""")
+    group.add_argument('-report_bleu', action='store_true',
+                       help="""Report bleu score after translation,
+                           call tools/multi-bleu.perl on command line""")
+    group.add_argument('-report_rouge', action='store_true',
+                       help="""Report rouge 1/2/3/L/SU4 score after translation
+                           call tools/test_rouge.py on command line""")
+
+    # Options most relevant to summarization.
+    group.add_argument('-dynamic_dict', action='store_true',
+                       help="Create dynamic dictionaries")
+    group.add_argument('-share_vocab', action='store_true',
+                       help="Share source and target vocabulary")
+
+    group = parser.add_argument_group('Beam')
+    group.add_argument('-fast', action="store_true",
+                       help="""Use fast beam search (some features may not be
+                           supported!)""")
+    group.add_argument('-beam_size', type=int, default=5,
+                       help='Beam size')
+    group.add_argument('-min_length', type=int, default=0,
+                       help='Minimum prediction length')
+    group.add_argument('-max_length', type=int, default=100,
+                       help='Maximum prediction length.')
+    group.add_argument('-max_sent_length', action=DeprecateAction,
+                       help="Deprecated, use `-max_length` instead")
+
+    # Alpha and Beta values for Google Length + Coverage penalty
+    # Described here: https://arxiv.org/pdf/1609.08144.pdf, Section 7
+    group.add_argument('-stepwise_penalty', action='store_true',
+                       help="""Apply penalty at every decoding step.
+                           Helpful for summary penalty.""")
+    group.add_argument('-length_penalty', default='none',
+                       choices=['none', 'wu', 'avg'],
+                       help="""Length Penalty to use.""")
+    group.add_argument('-coverage_penalty', default='none',
+                       choices=['none', 'wu', 'summary'],
+                       help="""Coverage Penalty to use.""")
+    group.add_argument('-alpha', type=float, default=0.,
+                       help="""Google NMT length penalty parameter
+                            (higher = longer generation)""")
+    group.add_argument('-beta', type=float, default=-0.,
+                       help="""Coverage penalty parameter""")
+    group.add_argument('-block_ngram_repeat', type=int, default=0,
+                       help='Block repetition of ngrams during decoding.')
+    group.add_argument('-ignore_when_blocking', nargs='+', type=str,
+                       default=[],
+                       help="""Ignore these strings when blocking repeats.
+                           You want to block sentence delimiters.""")
+    group.add_argument('-replace_unk', action="store_true",
+                       help="""Replace the generated UNK tokens with the
+                           source token that had highest attention weight. If
+                           phrase_table is provided, it will lookup the
+                           identified source token and give the corresponding
+                           target token. If it is not provided(or the identified
+                           source token does not exist in the table) then it
+                           will copy the source token""")
+
+    group = parser.add_argument_group('Logging')
+    group.add_argument('-verbose', action="store_true",
+                       help='Print scores and predictions for each sentence')
+    group.add_argument('-log_file', type=str, default="",
+                       help="Output logs to a file under this path.")
+    group.add_argument('-attn_debug', action="store_true",
+                       help='Print best attn for each word')
+    group.add_argument('-dump_beam', type=str, default="",
+                       help='File to dump beam information to.')
+    group.add_argument('-n_best', type=int, default=1,
+                       help="""If verbose is set, will output the n_best
+                           decoded sentences""")
+
+    group = parser.add_argument_group('Efficiency')
+    group.add_argument('-batch_size', type=int, default=30,
+                       help='Batch size')
+    group.add_argument('-gpu', type=int, default=-1,
+                       help="Device to run on")
+
 
 def translate_opts(parser):
     """ Translation / inference options """
